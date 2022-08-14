8cdb0c2729748a3133a20f53023301d6d74d683d
Subasini <110819730+subasinisamal98@users.noreply.github.com>
2022-08-14 20:45:07 +0530

    Add files via upload

---
+++ AtmMachine/src/project/java/ATM.java
@@ -0,0 +1,12 @@
+package project.java;
+
+import java.io.IOException;
+
+public class ATM extends optionmenu {
+	
+	 public static void main(String[] args) throws IOException{
+	        optionmenu optionMenu = new optionmenu();
+	        optionMenu.getLogin();
+	    }
+
+}
